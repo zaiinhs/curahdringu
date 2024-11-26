f3a2560633c031b5a01565b818aebbac8c377fbc
zaiinhs <zainalabidinhsc@gmail.com>
2024-11-27 02:49:35 +0700

    feat: adding map component

--- src/components/views/Home/index.tsx
+++ src/components/views/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/Layout";
 import HeroComponent from "./Fragments/Hero";
+import { MapsComponent } from "./Fragments/Maps";
 
 export const HomeView = () => {
   return (
     <Layout>
       <HeroComponent />
+      <MapsComponent />
     </Layout>
   );
 };
